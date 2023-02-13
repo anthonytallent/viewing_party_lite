403fb8d39e24451d56b58c2a4330d82d59767dfd
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-02-13 12:51:49 -0800

    Start adding login/authentication code

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   has_many :user_parties
   has_many :parties, through: :user_parties
 
-  validates_presence_of :name, :email
   validates_uniqueness_of :email
+  validates_presence_of :name, :email, :password
+
+  has_secure_password
 
   def hosted_parties
     parties.where(user_parties: {is_host: true})
